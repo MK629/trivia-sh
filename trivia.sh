d42ed9ec5eb34d761301829fda65eed0925f3f4e
mk <myatkw629@gmail.com>
2026-04-24 00:43:56 +0630

    done

--- trivia.sh
+++ trivia.sh
@@ -7,7 +7,7 @@ CATEGORY_NUMS=(10 11 12 15 17 20 21 23 31 32)
 DIFFICULTIES=("easy" "medium" "hard")
 
 #------------------[ Functions ]------------------
-isInputNumForCat() {
+isCatInputValid() {
     if [[ $1 =~ ^([1-9]|10)$ ]]; then
         return 0
     elif (( $1 < 0 || $1 > 10 )); then
@@ -24,7 +24,7 @@ isInputNumForCat() {
     fi
 }
 
-isInputNumForDiff() {
+isDiffInputValid() {
     if [[ $1 =~ ^([1-3])$ ]]; then
         return 0
     elif (( $1 < 0 || $1 > 3 )); then
@@ -38,6 +38,38 @@ isInputNumForDiff() {
         printf "| And no minuses as well!                         |\n"
         printf "+--------------------------------------------------+\n"
         return 1
+    fi
+}
+
+isAnsInputValid() {
+    if [[ $1 =~ ^([1-4])$ ]]; then
+        return 0
+    elif (( $1 < 0 || $1 > 4 )); then
+        printf "+--------------------------------------------------+\n"
+        printf "| Keep the number in range, mate!                  |\n"
+        printf "+--------------------------------------------------+\n"
+        return 1
+    else
+        printf "+--------------------------------------------------+\n"
+        printf "| Input must be a number, you bloody pillock!      |\n"
+        printf "| And no minuses as well!                          |\n"
+        printf "+--------------------------------------------------+\n"
+        return 1
+    fi
+}
+
+checkAnswer(){
+    CORRECT="$1"
+    CHOSEN="$2"
+
+    if [[ "$CHOSEN" == "$CORRECT" ]]; then
+        printf "+----------------------------------------------------------------------------------------------------------------+\n"
+        printf " THAT'S RIGHT! The answer is indeed: ${CHOSEN}.\n"
+        printf "+----------------------------------------------------------------------------------------------------------------+\n"
+    else
+        printf "+----------------------------------------------------------------------------------------------------------------+\n"
+        printf " NOPE! The actual answer is: ${CORRECT}.\n"
+        printf "+----------------------------------------------------------------------------------------------------------------+\n"
     fi
 }
 
@@ -71,11 +103,13 @@ do
         printf "%2d. %s\n" "$((i + 1))" "${CATEGORY_NAMES[i]}"
     done
 
-    read -p "Your choice: " categoryChoice
+    while true; do
+        read -p "Your choice: " categoryChoice
 
-    if ! isInputNumForCat "$categoryChoice"; then
-        continue
-    fi
+        if isCatInputValid "$categoryChoice"; then
+            break
+        fi
+    done
 
     ###[ Prompting for difficulties ]###
     printf "\n%s\n" "----------[ Select a category ]----------"
@@ -83,16 +117,51 @@ do
         printf "%2d. %s\n" "$((i + 1))" "${DIFFICULTIES[i]}"
     done
 
-    read -p "Your choice: " difficultyChoice
+    while true; do
+        read -p "Your choice: " difficultyChoice
 
-    if ! isInputNumForDiff "$difficultyChoice"; then
-        continue
-    fi
+        if isDiffInputValid "$difficultyChoice"; then
+            break
+        fi
+    done
 
-    ###[ Curling question ]###
-    QUESTION_JSON=$(curlTrivia ${CATEGORY_NUMS[$(($categoryChoice - 1))]} ${DIFFICULTIES[(($difficultyChoice - 1))]} | hxunent | jq ".results" )
+    ###[ Trivia curl and set variables ]###
+    QUESTION_JSON=$(curlTrivia ${CATEGORY_NUMS[$(($categoryChoice - 1))]} ${DIFFICULTIES[(($difficultyChoice - 1))]} | jq '.results[0]')
 
-    echo $QUESTION_JSON | jq
+    printf "%s\n" "============================================================\n"
+
+    QUESTION=$(printf "$QUESTION_JSON" | jq '.question' | hxunent)
+    CORRECT_ANS=$(printf "$QUESTION_JSON" | jq '.correct_answer' | hxunent)
+    ANSWERS=()
+    ANSWERS+=("$CORRECT_ANS")
+    ANSWERS+=("$(printf "$QUESTION_JSON" | jq '.incorrect_answers[0]' | hxunent)")
+    ANSWERS+=("$(printf "$QUESTION_JSON" | jq '.incorrect_answers[1]' | hxunent)")
+    ANSWERS+=("$(printf "$QUESTION_JSON" | jq '.incorrect_answers[2]' | hxunent)")
+
+    #Scramble answers
+    for i in "${!ANSWERS[@]}"; do
+        j=$((RANDOM % 4))
+        tmp="${ANSWERS[i]}"
+        ANSWERS[i]="${ANSWERS[j]}"
+        ANSWERS[j]="$tmp"
+    done
+
+    ###[ Ask question, prompt answer]###
+    printf "%s\n" "QUESTION: $QUESTION"
+
+    for i in "${!ANSWERS[@]}"; do
+        printf "%2d. %s\n" "$((i + 1))" "${ANSWERS[i]}"
+    done
+
+    while true; do
+        read -p "Choose your answer: " trivAns
+
+        if isAnsInputValid "$trivAns"; then
+            break
+        fi
+    done
+
+    checkAnswer "$CORRECT_ANS" "${ANSWERS[(($trivAns - 1))]}"
 
     ###[ Prompt quit]###
     while true;
@@ -104,7 +173,7 @@ do
         fi
     done
 
-    if [[ $quitAns == "Y" || $quitAns == "y" ]]; then
+    if [[ "$quitAns" == "Y" || "$quitAns" == "y" ]]; then
         QUIT=1
     fi
 done
